b8e967e3fec2c01ce18e3d9f6c418e4e7d77fd89
Mario Alberto Negrete Rodríguez <marioalbertonegreterodriguez@Marios-MacBook-Pro.local>
2021-05-14 13:06:27 -0500

    react

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,8 +1,23 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-
+import { ListItem, Avatar } from 'react-native-elements'
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+
+
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  }
+]
+
 
 export default function TabTwoScreen() {
   return (
@@ -10,7 +25,23 @@ export default function TabTwoScreen() {
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+
+      {
+    list.map((l, i) => (
+      <ListItem key={i} bottomDivider>
+        <Avatar source={{uri: l.avatar_url}} />
+        <ListItem.Content>
+          <ListItem.Title>{l.name}</ListItem.Title>
+          <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    ))
+  }
     </View>
+
+
+
+
   );
 }
 
