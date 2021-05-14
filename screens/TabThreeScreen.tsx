3800d4693d49397b23ade6d0ceee2178b94c55fa
Pedro Cruz <pedro.cruz@rocktech.mx>
2021-05-14 13:31:19 -0500

    Add Maps in Screen

--- screens/TabThreeScreen.tsx
+++ screens/TabThreeScreen.tsx
@@ -1,24 +1,14 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { StyleSheet, Dimensions  } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabThreeScreen() {
-  const obj = () => {
-    return { 
-      'cafe': 'americano',
-      'azucar_tipo': 'normal',
-      'azucar_cant': '2',
-      'leche':'null',
-      'dia': '22',
-      'mes' : '02',
-      'año':'2021',
-      'hora':'14:30'
-    }
-  }
   return (
     <View style={styles.container}>
+      <MapView style={styles.map} />
       <Text style={styles.title}>Tab Three</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabThreeScreen.tsx" />
@@ -40,5 +30,9 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
