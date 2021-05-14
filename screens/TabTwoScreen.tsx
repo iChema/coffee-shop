8ec57f593808fd53dbe78a71626ead853fe1b6da
Mario Alberto Negrete Rodríguez <marioalbertonegreterodriguez@Marios-MacBook-Pro.local>
2021-05-14 13:36:15 -0500

    lista de pedidos

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,42 +1,51 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-import { ListItem, Avatar } from 'react-native-elements'
+import { Pedido} from '../models/pedido';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 
-const list = [
+const orderList  : Array<Pedido> = [
   {
-    name: 'Amy Farha',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-    subtitle: 'Vice President'
-  },
-  {
-    name: 'Chris Jackson',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-    subtitle: 'Vice Chairman'
-  }
+    cafe : 'americano',
+    azucar_tipo:'normal',
+    azucar_cant: 2,
+    leche:'light',
+    dia: 22,
+    mes : 2,
+    anio: 2021,
+    hora:'14:30'
+    },
+    {
+      cafe : 'express',
+      azucar_tipo:'splendar',
+      azucar_cant: 1,
+      leche:'entera', 
+      dia: 21,
+      mes : 3,
+      anio: 2021,
+      hora:'15:30'
+      }
 ]
 
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-
+      <Text style={styles.title}>Lista de pedidos</Text>
       {
-    list.map((l, i) => (
-      <ListItem key={i} bottomDivider>
-        <Avatar source={{uri: l.avatar_url}} />
-        <ListItem.Content>
-          <ListItem.Title>{l.name}</ListItem.Title>
-          <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
-        </ListItem.Content>
-      </ListItem>
-    ))
-  }
+      orderList.map((order) => (
+      <View style={styles.item}>
+         <Text> {order.cafe}</Text>
+         <Text> {order.azucar_tipo}</Text>
+         <Text> {order.leche}</Text>
+         <Text> {order.dia}</Text>
+         <Text> {order.mes}</Text>
+         <Text> {order.anio}</Text>
+         <Text> {order.hora}</Text>
+      </View>
+      ))
+      }
     </View>
 
 
@@ -59,5 +68,10 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  item: {
+    marginVertical: 30,
+    height: '200px',
+    width: '100%',
   },
 });
